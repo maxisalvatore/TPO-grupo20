e82bb4f235e25b8c05f41f52ee3535952013b0c5
Maxi <maxi_salvatore@hotmail.com>
2023-05-12 18:13:42 -0300

    adicion de carousel

--- js/index.js
+++ js/index.js
@@ -4,3 +4,22 @@ const dropDownMenu = document.querySelector(".nav-menu")
 toggleHeader.onclick = function() {
     dropDownMenu.classList.toggle("open")
 }
+
+
+//Carousel
+const carousel    = document.querySelector('.carousel')
+const punto     = document.querySelectorAll('.punto')
+
+punto.forEach( ( listaPuntos , i )=> {
+    punto[i].addEventListener('click',()=>{
+        let posicion  = i
+        let movimiento = posicion * -(100/3)
+
+        carousel.style.transform = `translateX(${ movimiento }%)`
+
+        punto.forEach( ( listaPuntos , i )=>{
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
+    })
+})
